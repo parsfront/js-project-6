effdabdd70e77779509ec06c86e8e478c0180898
devbyali <ali.bahadoran@yahoo.com>
2022-01-20 19:45:23 +0330

    adding tempo

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ class DrumKit {
         this.isPlaying = null;
         this.selects = document.querySelectorAll("select");
         this.muteBtn = document.querySelectorAll(".mute");
+        this.tempoSlider = document.querySelector(".tempo-slider");
     }
     activePad() {
         this.classList.toggle("active");
@@ -111,6 +112,19 @@ class DrumKit {
             }
         }
     }
+    changeTempo(e) {
+        const tempoText = document.querySelector(".tempo-nr");
+        tempoText.innerText = e.target.value;
+    }
+    updateTempo(e) {
+        this.bpm = e.target.value;
+        clearInterval(this.isPlaying);
+        this.isPlaying = null;
+        const playBtn = document.querySelector(".play");
+        if (playBtn.classList.contains("active")) {
+            this.start();
+        }
+    }
 }
 const drumKit = new DrumKit();
 
@@ -135,4 +149,10 @@ drumKit.muteBtn.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         drumKit.mute(e);
     });
+});
+drumKit.tempoSlider.addEventListener("input", (e) => {
+    drumKit.changeTempo(e);
+});
+drumKit.tempoSlider.addEventListener("change", (e) => {
+    drumKit.updateTempo(e);
 });
